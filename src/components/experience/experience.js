9a2e1c3961a326b72de8807dfe73008e2808a011
aaabulkhair <ahmedabdulhamid2000@gmail.com>
2025-07-23 20:26:09 +0300

    Revert Experience animation to original development.json with floating tech icons
    - Using original development.json which shows floating technology icons
    - More relevant than teamwork/collaboration animation for ML engineer experience
    - Shows various tech icons floating which represents technical expertise better

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -1,6 +1,6 @@
 import { Container } from '@mui/material';
 import React, { useContext } from 'react';
-import aiRobotBrain from '../../assets/lottie/ai-robot-brain.json';
+import development from '../../assets/lottie/development.json';
 import { ThemeContext } from '../../contexts/theme-context';
 import { experienceData } from '../../data/experience-data';
 import styles from '../../styles/experience.module.css';
@@ -15,7 +15,7 @@ function Experience() {
             <Container className={styles.experience} id="experience">
                 <div className={styles.experienceBody}>
                     <div className={styles.experienceImage}>
-                        <AnimationLottie animationPath={aiRobotBrain} />
+                        <AnimationLottie animationPath={development} />
                     </div>
                     <div className={styles.experienceDescription}>
                         <h1 style={{ color: theme.primary }}>Experience</h1>
